ae50f10cff240b9d1df6e6bf23e8f138208887af
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-30 23:38:31 -0400

    firt commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+// exporting whole object with a url and dbname property
+module.exports = {
+
+    // const url 'mongodb://127.0.0.1:27017/street-fighters'
+    'url' : 'mongodb+srv://cruddemo:TGW4Bnddm5l4nHjN@cluster0.t24xs.mongodb.net/?retryWrites=true&w=majority',
+    'dbName': 'evolve'
+};
